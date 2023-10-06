87bfd69f2acff5100f78df50c852ccccb42a4326
Ety-ang <reoetyang@gmail.com>
2023-10-06 09:23:47 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
+
