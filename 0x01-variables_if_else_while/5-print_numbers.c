27fcfbfaa9cace089812e2f675132ef853741864
Ety-ang <reoetyang@gmail.com>
2023-10-06 09:14:54 -0700

    5-print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+}
+
